5a4c751922d5b1a8ed551592c643e927e7dfba98
Evgeniy <dev.shipitsyn@yandex.ru>
2024-11-19 18:36:30 +0300

    delete files

--- MyTasks/TasksList/TaskListPresenter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TaskListPresenter.swift
-//  MyTasks
-//
-//  Created by Евгений on 19.11.2024.
-//
-
-import Foundation
--- MyTasks/TasksList/TaskListRouter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TaskListRouter.swift
-//  MyTasks
-//
-//  Created by Евгений on 19.11.2024.
-//
-
-import Foundation
